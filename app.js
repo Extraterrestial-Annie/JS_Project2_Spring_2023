e29547401114841c8e0ec56169653577bc3221f1
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2023-04-30 21:03:41 +0300

    First state

---
+++ app.js
@@ -0,0 +1,13 @@
+var kinoFetch = new XMLHttpRequest();
+kinoFetch.open("GET","https://www.finnkino.fi/xml/Events/",true
+);
+kinoFetch.send();
+
+kinoFetch.onreadystatechange=function() {
+    if (kinoFetch.readyState==4 &&
+        kinoFetch.status==200){
+        document.getElementById("kino").innerHTML=
+        kinoFetch.responseText;
+    }
+    }
+    
