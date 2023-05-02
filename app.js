83ae3589c7eba918248f1c69275c8939c759ea41
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2023-05-02 18:54:51 +0300

    testing kino function

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ function kino() {
     var kinoFetch = new XMLHttpRequest();
     kinoFetch.open("GET","https://www.finnkino.fi/xml/Schedule/?area=1014&dt=02.05.2023",true
     );
-    kinoFetch.send();
+    kinoFetch.onreadystatechange=function() {
+        if (kinoFetch.readyState==4 &&
+            kinoFetch.status==200){
+            document.getElementById("kino").innerHTML=
+            kinoFetch.responseText;
+            console.log(kinoFetch.responseText)
+        }
+        }
 }
-kinoFetch.onreadystatechange=function() {
-    if (kinoFetch.readyState==4 &&
-        kinoFetch.status==200){
-        document.getElementById("kino").innerHTML=
-        kinoFetch.responseText;
-        console.log(kinoFetch.responseText)
-    }
-    }
-    
