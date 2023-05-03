b1f6d89320c87cab2f98d82951f992a993c8551b
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2023-05-03 19:24:35 +0300

    Not sure

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-function kino() {
-    var kinoFetch = new XMLHttpRequest();
-    kinoFetch.open("GET","https://www.finnkino.fi/xml/Events/?EventID=304123",true
+function theatres() {
+    var theatresFetch = new XMLHttpRequest();
+    theatresFetch.open("GET","https://www.finnkino.fi/xml/TheatreAreas/",true
     );
-    kinoFetch.send();
-    kinoFetch.onreadystatechange=function() {
-        if (kinoFetch.readyState==4 &&
-            kinoFetch.status==200){
-            document.getElementById("kino").innerHTML=
-            kinoFetch.responseText;
-            console.log(kinoFetch.responseText)
+    theatresFetch.send();
+    theatresFetch.onreadystatechange=function() {
+        if (theatresFetch.readyState==4 &&
+            theatresFetch.status==200){
+            document.getElementById("theatres").innerHTML=
+            theatresFetch.responseText;
+            console.log(theatresFetch.responseText)
+        }
+        }
+}
+
+function poster() {
+    var posterFetch = new XMLHttpRequest();
+    posterFetch.open("GET","http://media.finnkino.fi/1012/Event_13659/portrait_medium/65_1080.jpg",true
+    );
+    posterFetch.send();
+    posterFetch.onreadystatechange=function() {
+        if (posterFetch.readyState==4 &&
+            posterFetch.status==200){
+            document.getElementById("poster").innerHTML=
+            posterFetch.responseText;
+            console.log(posterFetch.responseText)
         }
         }
 }
