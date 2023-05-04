4bc1068463224e3e243613b75e16f68eacbd7090
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2023-05-04 18:41:34 +0300

    Trying hoverable list

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let dateNow = '${day}.${month}.${year}'
+
 function theatres() {
     var theatresFetch = new XMLHttpRequest();
     theatresFetch.open("GET","https://www.finnkino.fi/xml/TheatreAreas/",true
@@ -6,11 +14,28 @@ function theatres() {
     theatresFetch.onreadystatechange=function() {
         if (theatresFetch.readyState==4 &&
             theatresFetch.status==200){
-            document.getElementById("theatres").innerHTML=
-            theatresFetch.responseText;
-            console.log(theatresFetch.responseText)
+                var theatreIds = theatresFetch.responseText.getElementsByTagName("ID")
+                var theatreNames = theatresFetch.responseText.getElementsByTagName("Name")
+                var theatres = '<ul class="pure-menu-list">'+
+                '<li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">'+
+                    '<a href="#" id="menuLink1" class="pure-menu-link">Theatres</a>'+
+                    '<ul class="pure-menu-children">';
+                 
+                for (let i = 0; i < theatreIds.length; i++) {
+                    theatres += 
+                    '<li class="pure-menu-item">'+
+                        '<a href="#" class="pure-menu-link">' + theatreNames[i] + '</a>'+
+                    '</li>';
+                  }   
+                  
+
+                theatres +=    
+                    '</ul>'+
+                '</li>'+
+            '</ul>'
+                document.getElementById("theatres").innerHTML = theatres;
         }
-        }
+    }
 }
 
 function poster() {
